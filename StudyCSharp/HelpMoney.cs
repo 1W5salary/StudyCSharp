cdbe297fc0ced8fbca5f1a4917ef13a7d6c7e88f
侯滔 <627664697@qq.com>
2019-12-11 14:26:10 +0800

    帮帮币版块，定义一个类HelpMoney，包含你认为应该包含的字段和方法.

---
+++ StudyCSharp/HelpMoney.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudyCSharp
+{
+    public class HelpMoney
+    {
+        //帮帮币版块，定义一个类HelpMoney，包含你认为应该包含的字段和方法.
+        private User user { get; set; }
+        private int num { get; set; }
+    }
+}
