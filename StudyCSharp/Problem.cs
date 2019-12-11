8d66d12998d6605d09b5685d65cd3fae44c2551e
侯滔 <627664697@qq.com>
2019-12-11 14:15:11 +0800

    求助版块，定义一个类Problem

---
+++ StudyCSharp/Problem.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudyCSharp
+{
+    public class Problem
+    {
+        //求助版块，定义一个类Problem，包含字段：标题（Title）、正文（Body）、悬赏（Reward）、发布时间（PublishDateTime）和作者（Author）
+        //和方法Publish()
+        private string Tittle { get; set; }
+        private string Body { get; set; }
+        private int reward { get; set; }
+        private DateTime PublishDateTime { get; set; }
+        private User Author { get; set; }
+
+        public void Publish() { 
+            
+        }
+    }
+}
--- StudyCSharp/User.cs
+++ StudyCSharp/User.cs
@@ -6,6 +6,7 @@ namespace StudyCSharp
 {
     public class User
     {
+        //注册/登录功能，定义一个User类，包含字段：Name（用户名）、Password（密码）和 邀请人（InvitedBy），和方法：Register()、Login()
         private int UserId { get; set; }
         string UserName { get; set; }
         string password { get; set; }
