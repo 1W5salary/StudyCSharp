cc4a20b4bee8a12195d3763d6231f7cf9b8e9def
侯滔 <627664697@qq.com>
2019-12-11 14:34:40 +0800

    user.Password在类的外部只能改不能读

--- StudyCSharp/User.cs
+++ StudyCSharp/User.cs
@@ -8,8 +8,17 @@ namespace StudyCSharp
     {
         //注册/登录功能，定义一个User类，包含字段：Name（用户名）、Password（密码）和 邀请人（InvitedBy），和方法：Register()、Login()
         private int UserId { get; set; }
-        string UserName { get; set; }
-        string password { get; set; }
+        private string UserName { get; set; }
+        //user.Password在类的外部只能改不能读
+        private string _Password;
+        public string Password {
+            private get {
+                return _Password;
+            }
+            set {
+                _Password = value;            
+            }
+        }
         User InvitedBy { get; set; }
 
         public void Register(User user)
