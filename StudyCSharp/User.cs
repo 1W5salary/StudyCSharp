afcaf56c2ed5c0196235630bcd36b405e900cbf4
侯滔 <627664697@qq.com>
2019-12-11 14:04:44 +0800

    创建了项目,新建了User类

---
+++ StudyCSharp/User.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudyCSharp
+{
+    public class User
+    {
+        private int UserId { get; set; }
+        string UserName { get; set; }
+        string password { get; set; }
+        User InvitedBy { get; set; }
+
+        public void Register(User user)
+        {
+        }
+
+        public User Login(User user)
+        {
+            return null;
+        }
+    }
+}
