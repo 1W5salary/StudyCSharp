2bf8acf9abec1f77f76379567d947b8aeae47062
侯滔 <627664697@qq.com>
2019-12-28 10:39:46 +0800

    引入两个子类EmailMessage和DBMessage，和他们继承的接口ISendMessage

---
+++ StudyCSharp/DBMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudyCSharp
+{
+    class DBMessage : ISendMessage
+    {
+        public void Send()
+        {
+            Console.WriteLine("与数据库通信");
+        }
+    }
+}
---
+++ StudyCSharp/EmailMessage.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudyCSharp
+{
+    class EmailMessage : ISendMessage
+    {
+        public void Send()
+        {
+            Console.WriteLine("发送邮件消息");
+        }
+    }
+}
