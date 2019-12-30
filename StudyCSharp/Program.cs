32bb1355f398b36576778de118b16b17a0a4ab64
侯滔 <627664697@qq.com>
2019-12-30 12:08:02 +0800

    完成了课堂抛异常的示例

--- StudyCSharp/Program.cs
+++ StudyCSharp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Data.SqlClient;
 
 namespace StudyCSharp
 {
@@ -6,15 +7,77 @@ namespace StudyCSharp
     {
         static void Main(string[] args)
         {
-            string a = "源栈";
-            string b = "，";
-            string c = "欢迎您!";
+            //1.控制台读取用户输入,转换成int类型
+            //2.根据score生成等级
+            //3.根据等级输出输出评语
+            try
+            {
+                getRemark(getGrade(getSorce()));
+            }
 
-            //直接把abc连接起来
-            Console.WriteLine(string.Concat(a, b, c));
-            //把abc用' '连接起来
-            string joined = string.Join(' ', a, b, c);
-            Console.WriteLine(joined);   //注意空格：源栈 ， 欢迎您!
+            catch (ArgumentOutOfRangeException)
+            {
+                Console.WriteLine("你输入成绩的超出范围");
+                getRemark(getGrade(getSorce()));
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("你输入成绩不是int");
+                getRemark(getGrade(getSorce()));
+
+            }
+            
         }
+
+
+
+
+        static Grade getGrade(int score) {
+            if (score>100||score<0)
+            {
+                throw new ArgumentOutOfRangeException();
+            }
+            if (score > 80) 
+            {
+                return Grade.Excellent;
+            }
+            if (score>60)
+            {
+                return Grade.Passed;
+            }
+            else
+            {
+                return Grade.Failed;
+            }
+        }
+
+        static void getRemark(Grade grade) {
+            switch (grade)
+            {
+                case Grade.Excellent:
+                    Console.WriteLine("真厉害");
+                    break;
+                case Grade.Passed:
+                    Console.WriteLine("不错");
+                    break;
+                case Grade.Failed:
+                    Console.WriteLine("抱歉你没通过");
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException($"出现了未出现的{grade}");
+            }
+        }
+
+        static int getSorce()
+        {
+            return Convert.ToInt32(Console.ReadLine());
+        }
+
+        enum Grade { 
+            Excellent,
+            Passed,
+            Failed
+        }
+
     }
 }
